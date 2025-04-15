68cd109b1297cc8743b104160b432c2a3993a6f7
CHAUDRON Romain <romain.richard.chaudron@gmail.com>
2025-04-15 12:54:41 +0200

    Fixed search

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ const API_URLS = {
   GET_RATINGS: "https://pokeapi.crabdance.com/ratings",
   GET_RANDOM_STARTER: "https://pokeapi.crabdance.com/random-starter",
   GET_UNRATED_RANDOM_STARTER: "https://pokeapi.crabdance.com/random-unrated-starter",
-  SEARCH_STARTER: "https://pokeapi.crabdance.com/search-starter/",
+  SEARCH_STARTER: "https://pokeapi.crabdance.com/search-starter",
   GET_TOP_RATED: "https://pokeapi.crabdance.com/top-rated",
   GET_WORST_RATED: "https://pokeapi.crabdance.com/worst-rated",
   GET_TOTAL_VOTES: "https://pokeapi.crabdance.com/total-votes",
